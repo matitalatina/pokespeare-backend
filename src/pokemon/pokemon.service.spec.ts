fabdb45286d4861b9e31e16bdf8e49f46ef19d9a
Mattia Natali <matitalatina@gmail.com>
2020-07-18 12:40:29 +0200

    implemented pokemon service

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,68 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
+import { PokeApiClient } from './client/poke-api/poke-api.client';
+import { ShakespeareClient } from './client/shakespeare/shakespeare.client';
+import { mock, when, instance } from 'ts-mockito';
+import { getSpeciesResponse } from './client/poke-api/fixture/species';
+import { getTranslationResponse } from './client/shakespeare/fixture/translations';
+import { NotFoundException } from '@nestjs/common';
 
 describe('PokemonService', () => {
   let service: PokemonService;
+  const pokeApi = mock(PokeApiClient);
+  const shakespeare = mock(ShakespeareClient);
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PokemonService],
-    }).compile();
-
-    service = module.get<PokemonService>(PokemonService);
+    service = new PokemonService(
+      instance(pokeApi),
+      instance(shakespeare),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should reply with expected response', async () => {
+    const pokemon = 'pikachu';
+    const textToTranslate = 'translate';
+    const translatedText = 'translated';
+    when(pokeApi.getSpecies(pokemon)).thenResolve({
+      ...JSON.parse(getSpeciesResponse()),
+      flavor_text_entries: [
+        {
+          flavor_text: 'lingua italiana',
+          language: { name: 'it', url: '' },
+          version: { name: '', url: '' },
+        },
+        {
+          flavor_text: textToTranslate,
+          language: { name: 'en', url: '' },
+          version: { name: '', url: '' },
+        }]
+    });
+    when(shakespeare.translate(textToTranslate)).thenResolve(
+      {
+        ...JSON.parse(getTranslationResponse()),
+        contents: {
+          text: textToTranslate,
+          translated: translatedText,
+          translation: 'shakespeare',
+        }
+      }
+    );
+
+    expect(await service.getPokemon(pokemon)).toEqual({
+      name: pokemon,
+      description: translatedText,
+    });
+  });
+
+  it('should raise not found if english pokemon text is not found', async () => {
+    const pokemon = 'pikachu';
+    when(pokeApi.getSpecies(pokemon)).thenResolve({
+      ...JSON.parse(getSpeciesResponse()),
+      flavor_text_entries: []
+    });
+    expect(service.getPokemon(pokemon)).rejects.toThrow(NotFoundException);
   });
 });
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PokeApiClient } from './client/poke-api/poke-api.client';
+import { ShakespeareClient } from './client/shakespeare/shakespeare.client';
+
+export interface PokemonResponse {
+  name: string,
+  description: string,
+}
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  constructor(
+    private readonly pokeApi: PokeApiClient,
+    private readonly shakespeare: ShakespeareClient,
+  ) { }
+  async getPokemon(pokemon: string): Promise<PokemonResponse> {
+    const lowerPokemon = pokemon.toLocaleLowerCase();
+    const pokeResponse = await this.pokeApi.getSpecies(lowerPokemon);
+    const textToTranslate = pokeResponse.flavor_text_entries.find(f => f.language.name === 'en');
+    if (textToTranslate === undefined) {
+      throw new NotFoundException();
+    }
+    const shakespeareResponse = await this.shakespeare.translate(textToTranslate.flavor_text);
+    return {
+      name: lowerPokemon,
+      description: shakespeareResponse.contents.translated,
+    };
+  }
+}
