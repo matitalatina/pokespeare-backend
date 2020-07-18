24d76f9457874cd4461f8fd4ccd07e8a79fa3220
Mattia Natali <matitalatina@gmail.com>
2020-07-18 16:35:49 +0200

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.ALLOWED_ORIGINS) {
+    app.enableCors({ origin: process.env.ALLOWED_ORIGINS.split(',') });
+  }
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Listening on port ${port}`)
